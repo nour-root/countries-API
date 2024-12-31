b3c3a79982bc1c33756a1d3cd6c11c5dc691552b
Nour Mahmoud <nour.m.mahmod@gmail.com>
2024-12-31 10:49:39 +0300

    fixe tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@
 export default {
   content: ["./*.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
-    colors: {
-      dark_blue_Elements: "#2b3945",
-      Very_dark_blue_Mode_bg: "#202c37",
-      Very_dark_blue_Text_Light_Mode: "#111517",
-      Dark_gray_LightMode_inputs: "#858585",
-      Very_Light_Gray_bg_LightMode: "#fafafa",
-      White: "#fff",
+    extend: {
+      colors: {
+        dark_blue_Elements: "#2b3945",
+        Very_dark_blue_Mode_bg: "#202c37",
+        Very_dark_blue_Text_Light_Mode: "#111517",
+        Dark_gray_LightMode_inputs: "#858585",
+        Very_Light_Gray_bg_LightMode: "#fafafa",
+      },
     },
   },
   plugins: [],

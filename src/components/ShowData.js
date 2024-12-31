6982424f64210c9772292233d38a167cf0a9ebdf
Nour Mahmoud <nour.m.mahmod@gmail.com>
2024-12-31 10:28:19 +0300

    update

--- src/components/ShowData.js
+++ src/components/ShowData.js
@@ -1,7 +1,7 @@
 const ShowData = (country) => {
   return `<div
             id="${country.region}"
-            class="bg-White cursor-pointer hover:shadow-xl hover:-translate-y-1 duration-300 h-full drop-shadow-md max-sm:w-5/6 max-sm:mx-auto rounded-md overflow-hidden flex flex-col space-y-3"
+            class="bg-White cursor-pointer hover:shadow-xl hover:-translate-y-1 duration-300 h-full drop-shadow-md max-sm:mx-auto rounded-md overflow-hidden flex flex-col space-y-3"
           >
             <img src="${country.flags.png}" class="h-[55%]" alt="" />
             <div class="flex flex-col gap-2 px-6">

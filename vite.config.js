6910761b51211fa2f9e5404d73fe8cf6d2b2e193
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-01-08 02:34:18 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
-
+import Inspect from "vite-plugin-inspect";
 export default defineConfig({
+  plugins: [Inspect()],
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",      
+        main: "index.html",
         details: "details.html",
       },
     },
